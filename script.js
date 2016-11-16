f9dff8274516d1c391eca46001db782f292ea2c2
Mia Adjei <adjei.m@gmail.com>
2016-11-16 15:55:59 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,34 @@
+// var getModal = document.getElementById("modal");
+
+
+//After 3 seconds, changeVisibility of the modal
+// function showModal() {
+	setTimeout(changeVisibility, 1000);
+// }
+
+//Change the display of the modal from none to block
+function changeVisibility() {
+	var visibleModal = document.getElementById("modal");
+	visibleModal.style.visibility = "visible";
+}
+
+
+
+
+
+
+
+
+
+
+
+//If the user enters their email address, submit the email address and show "thank you".
+
+
+
+//Listen for a click on the x (span). When the x is clicked, display none again.
+function exitModal () {
+
+
+}
+
