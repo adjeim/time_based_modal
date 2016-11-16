9bdf45474c59bfc89dfa37c49bf8104fe69b6328
Mia Adjei <adjei.m@gmail.com>
2016-11-16 16:54:29 -0500

    Add hero image.

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-// var getModal = document.getElementById("modal");
-
 
 //After 3 seconds, changeVisibility of the modal
-// function showModal() {
-	setTimeout(changeVisibility, 1000);
-// }
+
+setTimeout(changeVisibility, 1000);
+
+
+var visibleModal = document.getElementsByClassName("modal")[0];
+
 
 //Change the display of the modal from none to block
 function changeVisibility() {
-	var visibleModal = document.getElementById("modal");
+
 	visibleModal.style.visibility = "visible";
 }
 
 
-
-
-
-
-
-
-
-
-
-//If the user enters their email address, submit the email address and show "thank you".
+//If the user enters their email address and clicks submit, the modal will show "thank you".
 
 
 
 //Listen for a click on the x (span). When the x is clicked, display none again.
-function exitModal () {
+var closeModalWindow = document.getElementById("close-modal");
+closeModalWindow.addEventListener("click", exitModal);
 
+function exitModal() {
+	visibleModal.style.visibility = "hidden";
 
 }
 
